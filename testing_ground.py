3de811ceca6cffe238489151e3c16a0864767310
trunghng <trung.skipper@gmail.com>
2022-11-05 21:10:51 +0700

    add vis

--- testing_ground.py
+++ testing_ground.py
@@ -3,16 +3,17 @@ from test_functions import rastrigin
 import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import trange
+from plot import Plot
 
 
 def test():
-    n_runs = 100
-    # n_runs = 1
-    np.random.seed(13)
+    np.random.seed(1)
+    # n_runs = 100
+    n_runs = 1
     # ns = [2, 5, 10, 20]
-    ns = [2, 5]
-    # popsizes = []
-    popsizes = [5, 7, 10, 14, 20, 32, 50]
+    ns = [2]
+    popsizes = [50]
+    # popsizes = [5, 7, 10, 14, 20, 32, 50]
 
     np.set_printoptions(precision=16)
     record = dict()
@@ -21,32 +22,33 @@ def test():
         for popsize in popsizes:
             fstop_count = 0
             for _ in trange(n_runs):
-                mu = np.random.uniform(1, 5, n)
-                A = np.identity(n)
+                # mu = np.random.uniform(1, 5, n)
+                # A = np.identity(n)
+                xstart = np.random.uniform(1, 5, n)
+                sigma = 2
                 ftarget = 1e-8
-                # figsize = (10, 10)
-                # img_basename = './images/cmaes-rastrigin'
-                # x1 = np.linspace(-500, 500, num=100)
-                # x2 = np.linspace(-500, 500, num=100)
+                figsize = (10, 10)
+                img_basename = './images/cmaes-rastrigin'
+                x1 = np.linspace(-10, 10, num=100)
+                x2 = np.linspace(-10, 10, num=100)
                 f = rastrigin
-                # plot = Plot(figsize, img_basename, x1, x2, f)
-                es_ = es.xNES(mu, A, ftarget=ftarget, popsize=popsize)
-                # es_ = es.CMAES(xstart, sigma)
-
+                plot = Plot(figsize, img_basename, x1, x2, f)
+                # es_ = es.xNES(mu, A, ftarget=ftarget, popsize=popsize)
+                es_ = es.CMAES(xstart, sigma, ftarget=ftarget, popsize=popsize)
 
                 while True:
                     X = es_.ask()
                     fitness_list = np.array([f(X[:, i]) for i in range(X.shape[1])])
                     es_.tell(fitness_list)
                     result = es_.result()
-                    # current_iter = result['count_iter']
-                    # if current_iter % 5 == 0:    
-                    #     plot.contour()
-                    #     plot.point(xstart, 'black')
-                    #     plot.point(result['best_sol'], 'red')
-                    #     plot.point(result['solutions'], 'blue', alpha=0.3)
-                    #     plot.save(f'iter{current_iter}')
-                    #     plot.clf()
+                    current_iter = result['count_iter']
+                    if current_iter % 5 == 0:    
+                        plot.contour()
+                        plot.point(xstart, 'black')
+                        plot.point(result['best_sol'], 'red')
+                        plot.point(result['solutions'], 'blue', alpha=0.3)
+                        plot.save(f'iter{current_iter}')
+                        plot.clf()
 
                     if es_.stop():
                         if result['best_val'] <= ftarget:
@@ -58,12 +60,12 @@ def test():
                         # print('count_eval:', result['count_eval'])
                         print('Terminated due to:', es_.stop())
                         # print(es_.result())
-                        # plot.contour()
-                        # plot.point(xstart, 'black')
-                        # plot.point(result['best_sol'], 'red')
-                        # plot.point(result['solutions'], 'blue', alpha=0.5)
-                        # plot.gif('end')
-                        # plot.close()
+                        plot.contour()
+                        plot.point(xstart, 'black')
+                        plot.point(result['best_sol'], 'red')
+                        plot.point(result['solutions'], 'blue', alpha=0.5)
+                        plot.gif('end')
+                        plot.close()
                         break
 
             print(f'n={n}, popsize={popsize}, fstop_count={fstop_count}')
@@ -143,4 +145,5 @@ def dim_popsize() -> None:
 if __name__ == '__main__':
     np.random.seed(13)
     dim_popsize()
+    # test()
     
